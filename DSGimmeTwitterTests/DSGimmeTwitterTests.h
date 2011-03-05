8a07aa73f684a4aac1f40a414f57569299ffe00e
dsmith <derek@simplegeo.com>
2011-03-05 15:27:26 -0800

    Initial Commit

---
+++ DSGimmeTwitterTests/DSGimmeTwitterTests.h
@@ -0,0 +1,17 @@
+//
+//  DSGimmeTwitterTests.h
+//  DSGimmeTwitterTests
+//
+//  Created by Derek Smith on 3/5/11.
+//  Copyright 2011 CrashCorp. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+
+@interface DSGimmeTwitterTests : SenTestCase {
+@private
+    
+}
+
+@end
---
+++ DSGimmeTwitterTests/DSGimmeTwitterTests.m
@@ -0,0 +1,33 @@
+//
+//  DSGimmeTwitterTests.m
+//  DSGimmeTwitterTests
+//
+//  Created by Derek Smith on 3/5/11.
+//  Copyright 2011 CrashCorp. All rights reserved.
+//
+
+#import "DSGimmeTwitterTests.h"
+
+
+@implementation DSGimmeTwitterTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in DSGimmeTwitterTests");
+}
+
+@end
